1975fd1b8c6a7b582f36ad7156e52522a0a3b79e
tejasness <tejasc888@gmail.com>
2023-02-06 14:42:27 +0000

    Feb 6, 2023, 8:12 PM

--- codes/week-2/functions/homework/find-no-is-even-or-odd.cpp
+++ codes/week-2/functions/homework/find-no-is-even-or-odd.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+void isEvenOrOdd(int no){
+  if(no%2==0){
+    cout<<"No is even"<<endl;
+  }
+  else{
+    cout<<"No is Odd"<<endl;
+  }
+}
+
 int main(){
   int n;
-  cout<<"Enter no";
-  
+  cout<<"Enter no"<<endl;
+  cin>>n;
+  isEvenOrOdd(n);
   return 0;
 }
