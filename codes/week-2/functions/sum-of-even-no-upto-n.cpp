faf3aea94a1024e951cfdabd3f9213fd4fd477cd
tejasness <tejasc888@gmail.com>
2023-02-06 13:59:49 +0000

    Feb 6, 2023, 7:29 PM

--- codes/week-2/functions/sum-of-even-no-upto-n.cpp
+++ codes/week-2/functions/sum-of-even-no-upto-n.cpp
@@ -3,17 +3,10 @@
 using namespace std;
 
 int getEvenSum(int n) {
-	cout << "inside getEveSum Fiunction" << endl;
-	cout << "Vlaue of n is " << n << endl;
-	
 	int sum = 0 ;
-	cout << "Initial value of sum is " << sum << endl;
 	for(int i = 2; i<=n; i = i + 2) {
-		cout << "for value of i: " << i << endl;
 		sum = sum + i;
-		cout << "sum now become: " << sum << endl;
 	}
-	cout << "returning sum = " << sum << endl;
 	return sum;
 }
 
