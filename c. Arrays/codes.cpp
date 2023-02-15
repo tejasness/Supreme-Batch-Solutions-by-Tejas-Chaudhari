67abd823bfcba5bc8305c75f8ff0f33a0566acb1
tejasness <tejasc888@gmail.com>
2023-02-15 12:50:00 +0000

    Feb 15, 2023, 6:20 PM

--- c. Arrays/codes.cpp
+++ c. Arrays/codes.cpp
@@ -78,24 +78,24 @@
 
 // => d. Maximum num in an array
 
-#include<iostream>
-#include<limits.h>
+// #include<iostream>
+// #include<limits.h>
 
-using namespace std;
+// using namespace std;
 
-int main(){
-	int arr[] = {2,4,6,1,3,7,9,12,56,43,21};
-	int size = 11;
-	//initialse the maxi variable with the 
-	//minimum possible integer value
-	int maxi = INT_MIN;
+// int main(){
+// 	int arr[] = {2,4,6,1,3,7,9,12,56,43,21};
+// 	int size = 11;
+// 	//initialse the maxi variable with the 
+// 	//minimum possible integer value
+// 	int maxi = INT_MIN;
 
-	for(int i=0; i<size; i++) {
-		if(arr[i] > maxi) {
-			//found a number gretaer than maxi, update maxi
-			maxi = arr[i] ;
-		}
-	}
+// 	for(int i=0; i<size; i++) {
+// 		if(arr[i] > maxi) {
+// 			//found a number gretaer than maxi, update maxi
+// 			maxi = arr[i] ;
+// 		}
+// 	}
 
-	cout << "maximum number is " << maxi  << endl;
-}
+// 	cout << "maximum number is " << maxi  << endl;
+// }
