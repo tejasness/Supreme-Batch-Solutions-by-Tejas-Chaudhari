16b6d8c27c74524ba4de062d4c8ff8b846535cde
tejasness <tejasc888@gmail.com>
2023-02-15 12:35:31 +0000

    Feb 15, 2023, 6:05 PM

--- c. Arrays/codes.cpp
+++ c. Arrays/codes.cpp
@@ -48,30 +48,54 @@
 
 // => c. Count 0s and 1s in array
 
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+// 		int arr[] = {0,1,1,1,0,0,0,0,1,0,1,0,1,0,1, 2, 4 ,5, 7};
+
+// 	int size = 19;
+
+// 	int numZero = 0;
+// 	int numOne = 0;
+
+// 	for(int i=0; i<size; i++) {
+// 		//if zero found, increment numZero
+// 		if(arr[i] ==0 ) {
+// 			numZero++;
+// 		}
+// 		//if One found, increment numOne
+// 		if(arr[i] == 1) {
+// 			numOne++;
+// 		}
+// 	}
+
+// 	cout << "number of zeroes " << numZero << endl;
+// 	cout << "number of ones "  << numOne << endl;
+// 	return 0;
+// }
+
+// => d. Maximum num in an array
+
 #include<iostream>
+#include<limits.h>
 
 using namespace std;
 
 int main(){
-		int arr[] = {0,1,1,1,0,0,0,0,1,0,1,0,1,0,1, 2, 4 ,5, 7};
-
-	int size = 19;
-
-	int numZero = 0;
-	int numOne = 0;
+	int arr[] = {2,4,6,1,3,7,9,12,56,43,21};
+	int size = 11;
+	//initialse the maxi variable with the 
+	//minimum possible integer value
+	int maxi = INT_MIN;
 
 	for(int i=0; i<size; i++) {
-		//if zero found, increment numZero
-		if(arr[i] ==0 ) {
-			numZero++;
-		}
-		//if One found, increment numOne
-		if(arr[i] == 1) {
-			numOne++;
+		if(arr[i] > maxi) {
+			//found a number gretaer than maxi, update maxi
+			maxi = arr[i] ;
 		}
 	}
 
-	cout << "number of zeroes " << numZero << endl;
-	cout << "number of ones "  << numOne << endl;
-	return 0;
+	cout << "maximum number is " << maxi  << endl;
 }
