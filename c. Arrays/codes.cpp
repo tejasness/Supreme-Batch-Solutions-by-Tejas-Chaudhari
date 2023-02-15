dbdac9afea25a510e88434cee13715890bca9a47
tejasness <tejasc888@gmail.com>
2023-02-15 12:25:36 +0000

    Feb 15, 2023, 5:55 PM

--- c. Arrays/codes.cpp
+++ c. Arrays/codes.cpp
@@ -47,3 +47,31 @@
 // }
 
 // => c. Count 0s and 1s in array
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+		int arr[] = {0,1,1,1,0,0,0,0,1,0,1,0,1,0,1, 2, 4 ,5, 7};
+
+	int size = 19;
+
+	int numZero = 0;
+	int numOne = 0;
+
+	for(int i=0; i<size; i++) {
+		//if zero found, increment numZero
+		if(arr[i] ==0 ) {
+			numZero++;
+		}
+		//if One found, increment numOne
+		if(arr[i] == 1) {
+			numOne++;
+		}
+	}
+
+	cout << "number of zeroes " << numZero << endl;
+	cout << "number of ones "  << numOne << endl;
+	return 0;
+}
