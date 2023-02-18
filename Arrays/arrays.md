7b503f776da9aaa1a9f1ab43668253a13273ba13
tejasness <tejasc888@gmail.com>
2023-02-18 14:22:14 +0000

    Feb 18, 2023, 7:52 PM

--- Arrays/arrays.md
+++ Arrays/arrays.md
@@ -196,5 +196,37 @@ int main(){
 ### g. Implement swap function 3 way
 
 ```cpp
+#include<iostream>
+using namespace std;
 
+void swap1(int a, int b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout<<a<<' '<<b<<'\n';
+}
+
+void swap2(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    cout<<a<<' '<<b<<'\n';
+}
+
+void swap3(int a, int b){
+    a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+    cout<<a<<' '<<b<<'\n';
+}
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+  swap3(a,b);
+  swap1(a,b);
+  swap2(a,b);
+
+    return 0;
+}
 ```
--- Arrays/codes.cpp
+++ Arrays/codes.cpp
@@ -159,36 +159,36 @@
 
 // => g. Implement swap function 3 way
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-void swap1(int a, int b){
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    cout<<a<<' '<<b<<'\n';
-}
+// void swap1(int a, int b){
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+//     cout<<a<<' '<<b<<'\n';
+// }
 
-void swap2(int a, int b){
-    int temp = a;
-    a = b;
-    b = temp;
-    cout<<a<<' '<<b<<'\n';
-}
+// void swap2(int a, int b){
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     cout<<a<<' '<<b<<'\n';
+// }
 
-void swap3(int a, int b){
-    a = a ^ b;
-		b = a ^ b;
-		a = a ^ b;
-    cout<<a<<' '<<b<<'\n';
-}
+// void swap3(int a, int b){
+//     a = a ^ b;
+// 		b = a ^ b;
+// 		a = a ^ b;
+//     cout<<a<<' '<<b<<'\n';
+// }
 
-int main(){
-	int a,b;
-	cin>>a>>b;
-  swap3(a,b);
-  swap1(a,b);
-  swap2(a,b);
+// int main(){
+// 	int a,b;
+// 	cin>>a>>b;
+//   swap3(a,b);
+//   swap1(a,b);
+//   swap2(a,b);
 
-    return 0;
-}
+//     return 0;
+// }
